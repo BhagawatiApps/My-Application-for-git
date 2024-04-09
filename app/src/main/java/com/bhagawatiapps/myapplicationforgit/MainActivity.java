283fc7159fc693e4427b6c00bf9c7d203559a5a8
BhagawatiApps <bhagawatiapps@gmail.com>
2024-04-09 19:37:54 +0530

    Initial commit

--- app/src/main/java/com/bhagawatiapps/myapplicationforgit/MainActivity.java
+++ app/src/main/java/com/bhagawatiapps/myapplicationforgit/MainActivity.java
@@ -1,6 +1,10 @@
 package com.bhagawatiapps.myapplicationforgit;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
